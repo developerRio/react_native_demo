cabd79eea913a887cc52d84284bb3045c74afc2b
Himanshu Raj <34713120+developerRio@users.noreply.github.com>
2020-05-06 17:42:31 +0530

    Working on List View

--- src/screens/Home/TabPages/Pizzas.js
+++ src/screens/Home/TabPages/Pizzas.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import styles from './styles'
+import { recipes, categories } from '../../../data/DataArrays'
+import { FlatList, TouchableHighlight } from 'react-native-gesture-handler'
+
 
 export default class PizzaPage extends Component{
 
+
+    renderRecipes = ({ item }) => (
+      <TouchableHighlight underlayColor='#00000000' onPress={() => console.log("Item Clicked")}>
+        <View style={styles.container}>
+          <Image style={styles.photo} source={{ uri: item.photo_url }} />
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.category}>{getCategoryName(item.categoryId)}</Text>
+        </View>
+      </TouchableHighlight>
+    )
+  
     render(){
       return(
         <View style={styles.parentContainer}>
+          <FlatList
+            vertical
+            showsVerticalScrollIndicator={true}
+            numColumns={1}
+            data={recipes}
+            renderItem={this.renderRecipes}
+            keyExtractor={item => `${item.recipeId}`}
+          />  
 
         </View>
       )
 
     }
 
+}
+
+function getCategoryName(categoryId) {
+  let name;
+  categories.map(data => {
+    if (data.id == categoryId) {
+      name = data.name;
+    }
+  });
+  return name;
 }
--- src/screens/Home/TabPages/styles.js
+++ src/screens/Home/TabPages/styles.js
@@ -1,4 +1,13 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
+
+// screen sizing
+const { width, height } = Dimensions.get('window');
+// orientation must fixed
+const SCREEN_WIDTH = width < height ? width : height;
+
+const recipeNumColums = 2;
+// item size
+const RECIPE_ITEM_HEIGHT = 150;
 
 const styles = StyleSheet.create({
   
@@ -92,6 +101,43 @@ toolbarContainerViewStyle:{
     backgroundColor:'#2E77B5', 
     width:'100%',  
   },
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf:'auto',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 20,
+    width: '90%',  // (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) / recipeNumColums
+    height: RECIPE_ITEM_HEIGHT + 75,
+    borderColor: '#eee',
+    backgroundColor:'#fff',
+    borderWidth: 0.5,
+    borderRadius: 15
+  },
+
+  photo: {
+    width: '100%',  // (SCREEN_WIDTH - (recipeNumColums + 1) * RECIPE_ITEM_MARGIN) / recipeNumColums
+    height: RECIPE_ITEM_HEIGHT,
+    borderRadius: 15,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0
+  },
+  title: {
+    flex: 1,
+    fontSize: 17,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#444444',
+    marginTop: 3,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  category: {
+    marginTop: 5,
+    marginBottom: 5
+  }
 
 })
 
